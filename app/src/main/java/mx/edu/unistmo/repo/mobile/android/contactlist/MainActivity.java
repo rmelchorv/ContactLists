59849f3e11cee29bbe28ce6453f58b6ff493b83a
rmelchorv <rmelchorv@gmail.com>
2022-05-06 17:18:21 -0500

    RecyclerView completed!

--- app/src/main/java/mx/edu/unistmo/repo/mobile/android/contactlist/MainActivity.java
+++ app/src/main/java/mx/edu/unistmo/repo/mobile/android/contactlist/MainActivity.java
@@ -22,9 +22,18 @@ public class MainActivity extends AppCompatActivity {
 
         /* Setting contact list */
         ArrayList<Contact> contacts = new ArrayList<>();
-        contacts.add(new Contact("Juan Perez","9710000001","juan.perez@gmail.com"));
-        contacts.add(new Contact("Luis Lopez","9710000002","luis.lopez@gmail.com"));
-        contacts.add(new Contact("Lalo Gomez","9710000003","lalo.gomez@gmail.com"));
+        contacts.add(new Contact("Juan Penas","9710000001","juan.penas@gmail.com"));
+        contacts.add(new Contact("Lalo Lopez","9710000002","lalo.lopez@gmail.com"));
+        contacts.add(new Contact("Paco Gomez","9710000003","paco.gomez@gmail.com"));
+        contacts.add(new Contact("John Senna","9710000004","john.senna@gmail.com"));
+        contacts.add(new Contact("Patt Games","9710000005","patt.games@gmail.com"));
+        contacts.add(new Contact("Alan Horne","9710000006","alan.horne@gmail.com"));
+        contacts.add(new Contact("Pepe Pecas","9710000007","pepe.pecas@gmail.com"));
+        contacts.add(new Contact("Hugo Sante","9710000008","hugo.sante@gmail.com"));
+        contacts.add(new Contact("Paco Picas","9710000009","paco.picas@gmail.com"));
+        contacts.add(new Contact("Luis Aloro","9710000010","luis.aloro@gmail.com"));
+        contacts.add(new Contact("Beth Owner","9710000011","beth.owner@gmail.com"));
+        contacts.add(new Contact("Dart Vader","9710000012","dart.vader@gmail.com"));
 
         ArrayList<String> names = new ArrayList<>();
         for (Contact contact : contacts)
