f5c3d4b5240fd6b3435f4aab31f0750413068b21
rmelchorv <rmelchorv@gmail.com>
2022-05-13 12:42:35 -0500

    ViewPager!

--- app/src/main/java/mx/edu/unistmo/repo/mobile/android/contactlist/MainActivity.java
+++ app/src/main/java/mx/edu/unistmo/repo/mobile/android/contactlist/MainActivity.java
@@ -22,16 +22,16 @@ public class MainActivity extends AppCompatActivity {
 
         /* Setting contact list */
         ArrayList<ContactModel> contacts = new ArrayList<>();
-        contacts.add(new ContactModel("Juan Penas","9710000001","juan.penas@gmail.com"));
+        contacts.add(new ContactModel("Juan Pears","9710000001","juan.penas@gmail.com"));
         contacts.add(new ContactModel("Lalo Lopez","9710000002","lalo.lopez@gmail.com"));
         contacts.add(new ContactModel("Paco Gomez","9710000003","paco.gomez@gmail.com"));
         contacts.add(new ContactModel("John Senna","9710000004","john.senna@gmail.com"));
-        contacts.add(new ContactModel("Patt Games","9710000005","patt.games@gmail.com"));
+        contacts.add(new ContactModel("Pats Games","9710000005","patt.games@gmail.com"));
         contacts.add(new ContactModel("Alan Horne","9710000006","alan.horne@gmail.com"));
-        contacts.add(new ContactModel("Pepe Pecas","9710000007","pepe.pecas@gmail.com"));
+        contacts.add(new ContactModel("Pepe Pecan","9710000007","pepe.pecas@gmail.com"));
         contacts.add(new ContactModel("Hugo Sante","9710000008","hugo.sante@gmail.com"));
-        contacts.add(new ContactModel("Paco Picas","9710000009","paco.picas@gmail.com"));
-        contacts.add(new ContactModel("Luis Aloro","9710000010","luis.aloro@gmail.com"));
+        contacts.add(new ContactModel("Paco Pitch","9710000009","paco.picas@gmail.com"));
+        contacts.add(new ContactModel("Luis Adore","9710000010","luis.aloro@gmail.com"));
         contacts.add(new ContactModel("Beth Owner","9710000011","beth.owner@gmail.com"));
         contacts.add(new ContactModel("Dart Vader","9710000012","dart.vader@gmail.com"));
 
