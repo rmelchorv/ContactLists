3202c3af0145871da01e63a64589b0537bb0d461
rmelchorv <rmelchorv@gmail.com>
2022-05-16 17:53:02 -0500

    Permission non func or deprecated!

--- app/src/main/java/mx/edu/unistmo/repo/mobile/android/contactlist/view/TestActivity.java
+++ app/src/main/java/mx/edu/unistmo/repo/mobile/android/contactlist/view/TestActivity.java
@@ -1,9 +1,17 @@
 package mx.edu.unistmo.repo.mobile.android.contactlist.view;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +24,10 @@ import mx.edu.unistmo.repo.mobile.android.contactlist.view.fragment.DatePickerFr
 
 public class TestActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE = 1;
+    private static final int PERMISSION_ENABLE_REQUEST_CODE = 0;
+
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +49,37 @@ public class TestActivity extends AppCompatActivity {
 
             newFragment.show(getSupportFragmentManager(), "datePicker");
         });
-    }
+
+        Button btnBluetooth = findViewById(R.id.btnBluetooth);
+
+        btnBluetooth.setOnClickListener(view -> {
+
+            requestBluetoothPermission();
+
+            //getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)
+/*
+            BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+
+            if(adapter == null)
+                Toast.makeText(this, "There is no bluetooth device found!", Toast.LENGTH_SHORT).show();
+            else if (!adapter.isEnabled()) {
+                Intent i = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+
+                //startActivityIfNeeded(i, PERMISSION_ENABLE_REQUEST_CODE);
+
+                ActivityResultLauncher<String> mGetContent = registerForActivityResult(
+                    new ActivityResultContracts.RequestPermission(), isGranted -> {
+                        if (isGranted)
+                        {
+                            //do something
+                        }
+                });
+
+                mGetContent.launch(Manifest.permission.BLUETOOTH);
+            }
+            */
+        });
+     }
 
     @SuppressLint("NonConstantResourceId")
     public void inflateMenuPopUp(View view) {
@@ -66,5 +108,32 @@ public class TestActivity extends AppCompatActivity {
         });
 
         popupMenu.show();
+    }
+
+    /* Check if permission is declared */
+    public boolean isBluetoothPermissionGranted() {
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH);
+
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    /* Check if permission is enabled */
+    public void requestBluetoothPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.BLUETOOTH))
+            Toast.makeText(this, "Permission has already granted!", Toast.LENGTH_SHORT).show();
+        else
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.BLUETOOTH }, REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (isBluetoothPermissionGranted())
+                    Toast.makeText(this, "Permission has granted!", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
