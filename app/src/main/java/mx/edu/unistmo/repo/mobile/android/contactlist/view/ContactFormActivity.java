e92ac4888890ffcf458e5935f4e3014fe72a245d
rmelchorv <rmelchorv@gmail.com>
2022-05-20 10:41:16 -0500

    Shared prefs done!

--- app/src/main/java/mx/edu/unistmo/repo/mobile/android/contactlist/view/ContactFormActivity.java
+++ app/src/main/java/mx/edu/unistmo/repo/mobile/android/contactlist/view/ContactFormActivity.java
@@ -2,10 +2,13 @@ package mx.edu.unistmo.repo.mobile.android.contactlist.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -27,21 +30,48 @@ public class ContactFormActivity extends AppCompatActivity {
         Button btnSave = findViewById(R.id.btnSave);
 
         btnSave.setOnClickListener(view -> {
-            try {
-                EditText etName = findViewById(R.id.etName);
-                FileOutputStream os = openFileOutput("contact.txt", Context.MODE_APPEND);
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                    os.write(etName.getText().toString().getBytes(StandardCharsets.UTF_8));
-                }
-
-                os.close();
-                etName.setText("");
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            saveFileIO();
+            saveSharedPrefs();
+            getSharedPrefs();
         });
+    }
+
+    public void saveFileIO() {
+        try {
+            EditText etName = findViewById(R.id.etName);
+            FileOutputStream os = openFileOutput("contact.txt", Context.MODE_APPEND);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                os.write(etName.getText().toString().getBytes(StandardCharsets.UTF_8));
+            }
+
+            os.close();
+            etName.setText("");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveSharedPrefs() {
+        SharedPreferences sp = getSharedPreferences("contacts", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+
+        EditText etName = findViewById(R.id.etName);
+        EditText etTelephone = findViewById(R.id.etTelephone);
+
+        editor.putString("name", etName.getText().toString());
+        editor.putString("telephone", etTelephone.getText().toString());
+        editor.apply();
+    }
+
+    private void getSharedPrefs() {
+        SharedPreferences sp = getSharedPreferences("contacts", Context.MODE_PRIVATE);
+
+        String name = sp.getString("name", "");
+        String telephone = sp.getString("telephone", "");
+
+        Toast.makeText(this, name + " | " + telephone, Toast.LENGTH_SHORT).show();
     }
 }
